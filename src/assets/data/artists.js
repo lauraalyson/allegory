bca3504f5aa96677b18802e76a48c1ee12727ba3
lauraalyson <lauraalyson3@gmail.com>
2021-12-02 11:55:32 -0500

    add artist data and images

---
+++ src/assets/data/artists.js
@@ -0,0 +1,154 @@
+const artists = [
+  {
+    id: 1,
+    name: 'Michelangelo',
+    artistImage: './../images/michelangelo/michelangelo5.jpg',
+    description:
+    'Michelangelo Buonarroti (1475–1564), a towering genius in the history of Western art, is the subject of this once-in-a-lifetime exhibition. During his long life, Michelangelo was celebrated for the excellence of his disegno, the power of drawing and invention that provided the foundation for all the arts. For his mastery of drawing, design, sculpture, painting, and architecture, he was called Il Divino ("the divine one") by his contemporaries. His powerful imagery and dazzling technical virtuosity transported viewers and imbued all of his works with a staggering force that continues to enthrall us today.',
+    origin: 'Rome, Italy',
+    date: '(1475–1564)',
+    style: 'Renaissance',
+    audioDescription:
+    'This exhibition presents a stunning range and number of works by the artist: 133 of his drawings, three of his marble sculptures, his earliest painting, his wood architectural model for a chapel vault, as well as a substantial body of complementary works by other artists for comparison and context. Among the extraordinary international loans are the complete series of masterpiece drawings he created for his friend Tommaso de Cavalieri and a monumental cartoon for his last fresco in the Vatican Palace. Selected from 50 public and private collections in the United States and Europe, the exhibition examines Michelangelos rich legacy as a supreme draftsman and designer.',
+    artwork: {
+      artwork1: {
+        title: 'The Torment of Saint Anthony',
+        date: 'ca. 1487–88',
+        medium: 'Tempera and oil on wood',
+        image: './../images/michelangelo/michelangelo6.jpeg',
+        audio: 'https://soundcloud.com/metmuseum/601-torment-of-st-anthony'
+      },
+      artwork2: {
+        title: 'Madonna and Child',
+        date: 'ca. 1497',
+        medium: 'Tempera on panel',
+        image: './../images/michelangelo/michelangelo4.jpg',
+        audio: 'https://soundcloud.com/metmuseum/611-madonna-and-child'
+      },
+      artwork3: {
+        title: 'Fall of Phaeton',
+        date: 'ca. 1533',
+        medium: 'Black chalk',
+        image: './../images/michelangelo/michelangelo3.jpg',
+        audio: 'https://soundcloud.com/metmuseum/617-fall-of-phaeton'
+      },
+      artwork4: {
+        title: 'Portrait of Andrea Quaratesi',
+        date: 'ca. 1532',
+        medium: 'Black chalk',
+        image: './../images/michelangelo/michelangelo1.jpg',
+        audio: 'https://soundcloud.com/metmuseum/610-portrait-of-andrea'
+      },
+      artwork5: {
+        title: 'Bacchanal of Children',
+        date: 'ca. 1533',
+        medium: 'Red chalk',
+        image: './../images/michelangelo/michelangelo2.jpg',
+        audio: 'https://soundcloud.com/metmuseum/616-bacchanal-of-children'
+      }
+    }
+  },
+
+  {
+    id: 2,
+    name: 'Valentin de Boulonge',
+    artistImage: './../images/valentin/valentin3.jpg',
+    description:
+    'The greatest French follower of Caravaggio (1571–1610), Valentin de Boulogne (1591–1632) was also one of the outstanding artists in 17th-century Europe. In the years following Caravaggios death, he emerged as one of the most original protagonists of the new, naturalistic painting.',
+    origin: 'Coulommiers, France',
+    date: '(1591–1632)',
+    style: 'Tenebrist Style',
+    audioDescription:
+    'This is the first monographic exhibition devoted to Valentin, who is little known because his career was short-lived—he died at age 41—and his works are so rare. Around 60 paintings by Valentin survive, and this exhibition brings together 45 of them, with works coming from Rome, Vienna, Munich, Madrid, London, and Paris. Exceptionally, the Musée du Louvre, which possesses the most important and extensive body of Valentins works, is lending all of its paintings by the artist.',
+    artwork: {
+      artwork1: {
+        title: 'Denial of Saint Peter',
+        date: 'ca. 1615–17',
+        medium: 'Oil on canvas',
+        image: './../images/valentin/valentin6.jpg',
+        audio: 'hhttps://soundcloud.com/metmuseum/001-cut-2'
+      },
+      artwork2: {
+        title: 'Judith with the Head of Holofernes',
+        date: 'ca. 1626–27',
+        medium: 'Oil on canvas',
+        image: './../images/valentin/valentin3.jpg',
+        audio: 'https://soundcloud.com/metmuseum/019-cut-3'
+      },
+      artwork3: {
+        title: 'Martyrdom of Saints Processus and Martinian',
+        date: 'ca. 1629–30',
+        medium: 'Oil on canvas',
+        image: './../images/valentin/valentin4.jpg',
+        audio: 'https://soundcloud.com/metmuseum/020-cut-3'
+      },
+      artwork4: {
+        title: 'Lute Player',
+        date: 'ca. 1625–26',
+        medium: 'Oil on canvas',
+        image: './../images/valentin/valentin1.jpg',
+        audio: 'https://soundcloud.com/metmuseum/011-cut-3'
+      },
+      artwork5: {
+        title: 'Fortune-Teller',
+        date: 'ca. 1626–28',
+        medium: 'Oil on canvas',
+        image: './../images/valentin/5',
+        audio: 'https://soundcloud.com/metmuseum/012-cut-3'
+      }
+    }
+  },
+
+  {
+    id: 3,
+    name: 'Vigée Le Brun',
+    artistImage: './../images/vigee/vigee1.jpg',
+    description:
+    'Elisabeth Louise Vigée Le Brun (French, 1755–1842) is one of the finest 18th-century French painters and among the most important of all women artists. An autodidact with exceptional skills as a portraitist, she achieved success in France and Europe during one of the most eventful, turbulent periods in European history.',
+    origin: 'Paris, France',
+    date: '(1755–1842)',
+    style: 'Rococo, Neoclassicism',
+    audioDescription:
+    'Elisabeth Louise Vigée Le Brun (French, 1755–1842) was remarkable not only for her technical gifts but for her understanding of and sympathy with her sitters. This is the first retrospective and only the second exhibition devoted to Vigée Le Brun in modern times. The 80 works on view include paintings and a few pastels from European and American public and private collections.',
+    artwork: {
+      artwork1: {
+        title: 'Marie Antoinette in Court Dress',
+        date: 'ca. 1778',
+        medium: 'Oil on canvas',
+        image: './../images/vigee/vigee6.jpg',
+        audio: 'https://soundcloud.com/metmuseum/001-cut-2'
+      },
+      artwork2: {
+        title: 'Allegory of Poetry',
+        date: 'ca. 1774',
+        medium: 'Oil on canvas',
+        image: './../images/vigee/vigee2.jpg',
+        audio: 'https://soundcloud.com/metmuseum/003-cut-2'
+      },
+      artwork3: {
+        title: 'Peace Bringing Back Abundance',
+        date: 'ca. 1780',
+        medium: 'Oil on canvas',
+        image: './../images/vigee/vigee3.jpg',
+        audio: 'https://soundcloud.com/metmuseum/004-cut-2'
+      },
+      artwork4: {
+        title: 'Julie Le Brun as Flora',
+        date: 'ca. 1799',
+        medium: 'Oil on canvas',
+        image: './../images/vigee/vigee4.jpg',
+        audio: 'https://soundcloud.com/metmuseum/022-cut-2'
+      },
+      artwork5: {
+        title: 'Madame Royale and the Dauphin Seated in a Garden',
+        date: 'ca. 1784',
+        medium: 'Oil on canvas',
+        image: './../images/vigee/vigee5.jpg',
+        audio: 'https://soundcloud.com/metmuseum/010-cut-2'
+      }
+    }
+  }
+]
+
+const artistData = { artists }
+export default artistData
