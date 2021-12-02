280040e63cbc12a037ea42b1d99d35e5ae5126c1
lauraalyson <lauraalyson3@gmail.com>
2021-12-02 14:06:56 -0500

    add new fonts, map through data

--- src/components/Pages/Main.js
+++ src/components/Pages/Main.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+import artists from './../../assets/data/artists'
+// import ReactAudioPlayer from 'react-audio-player'
 import '././../../index.scss'
 
 class Main extends Component {
@@ -10,9 +12,37 @@ class Main extends Component {
   }
 
   render () {
+    const images = artists.map((artist) => (
+      <img key={artist} src={require(`./../../assets/images/${artist.artistImage}`)} className='image-responsive'/>
+    ))
+
     return (
       <div>
-        <p>New Screen</p>
+        {artists.map((artist) => (
+          <div className='artist-info' key={artist.id}>
+            <h2>{`${artist.name}`}</h2>
+            <img src={images[artist.id - 1]}/>
+            <p>
+              {artist.origin}<br />
+              {artist.date}<br />
+              {artist.style}<br />
+            </p>
+            <p>{artist.description}</p>
+
+            {artist.artwork.map((artwork) => (
+              <div key={artwork.id}>
+                <h4>{artwork.title}</h4>
+                <p>
+                  {artwork.date}<br />
+                  {artwork.medium}<br />
+                  {artwork.image}<br />
+                  {artwork.audio}<br />
+                </p>
+              </div>
+            ))}
+
+          </div>
+        ))}
       </div>
     )
   }
